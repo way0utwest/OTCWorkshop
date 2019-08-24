6d4b4f681431f3455e6497fe063d615851d56510
Steve Jones <sjones@sqlservercentral.com>
2019-08-23 18:15:54 -0600

    Completed search procedure

--- 00-4-Prompt_Setup.sql
+++ 00-4-Prompt_Setup.sql
@@ -6,14 +6,16 @@ Database: SimpleTalkDev
 Copyright 2019 Redgate Software
 */
 
-CREATE PROCEDURE GetLatestBlogForAuthor
-  @authorid INT
-  AS
-  BEGIN
-	SELECT TOP 1 *
-	 FROM dbo.Articles AS a INNER JOIN dbo.Contacts AS c ON c.ContactsID = a.AuthorID
-	 WHERE a.AuthorID = @authorid
-  END
+CREATE PROCEDURE GetLatestBlogForAuthor @authorid INT
+AS
+BEGIN
+    SELECT TOP 1
+           *
+    FROM dbo.Articles AS a
+        INNER JOIN dbo.Contacts AS c
+            ON c.ContactsID = a.AuthorID
+    WHERE a.AuthorID = @authorid;
+END;
 GO
 
 
@@ -28,14 +30,15 @@ Create Custom table according to our standards
 
 
 */
-CREATE TABLE MyTable (
- MyTableKey INT IDENTITY(1,1) NOT NULL CONSTRAINT MyTablePK PRIMARY KEY
- , 
-)
+CREATE TABLE MyTable
+(
+    MyTableKey INT IDENTITY(1, 1) NOT NULL
+        CONSTRAINT MyTablePK PRIMARY KEY,
+);
 GO
 CREATE OR ALTER PROCEDURE dbo.PaymentReportForArticles
-   @startdate DATE
-   , @enddate date
+    @startdate DATE,
+    @enddate DATE
 AS
 SELECT ContactID,
        COUNT(ArticleID) AS NumberOfArticles,
@@ -53,26 +56,37 @@ WHERE PublishDate >= @startdate
 GROUP BY ContactID;
 
 GO
-CREATE PROCEDURE SearchArticles (@AuthorName VARCHAR(200) = NULL, @ArticleTitle VARCHAR(142) = NULL, @ArticleDesc varchar(1000) = NULL, @ReadingTimeMax int = NULL)
+CREATE PROCEDURE SearchArticles
+(
+    @AuthorName VARCHAR(200) = NULL,
+    @ArticleTitle VARCHAR(142) = NULL,
+    @ArticleDesc VARCHAR(1000) = NULL,
+    @ReadingTimeMax INT = NULL
+)
 AS
-DECLARE @sSQL NVARCHAR(2000), @Where NVARCHAR(1000) = ''
-SET @sSQL = 'SELECT ProductID, ReferenceOrderID, TransactionType, Quantity, TransactionDate, ActualCost
-from dbo.Articles as a inner join dbo.Contacts as c on a.authorid = c.contactsid '
- 
-IF @AuthorName is not null
-SET @Where = @Where + 'AND c.ContactFullName like @_Author '
-IF @OrderID is not null
-SET @Where = @Where + 'AND ReferenceOrderID = @_OrderID '
-IF @TransactionType IS NOT NULL
-SET @Where = @Where + 'AND TransactionType = @_TransactionType '
-IF @Qty IS NOT NULL
-SET @Where = @Where + 'AND Quantity = @_Qty '
- 
+DECLARE @sSQL NVARCHAR(2000),
+        @Where NVARCHAR(1000) = N'';
+SET @sSQL
+    = N'SELECT ProductID, ReferenceOrderID, TransactionType, Quantity, TransactionDate, ActualCost
+from dbo.Articles as a inner join dbo.Contacts as c on a.authorid = c.contactsid ';
+
+IF @AuthorName IS NOT NULL
+    SET @Where = @Where + N'AND c.ContactFullName like @_Author ';
+IF @ArticleTitle IS NOT NULL
+    SET @Where = @Where + N'AND a.title = @_ArticleTitle ';
+IF @ArticleDesc IS NOT NULL
+    SET @Where = @Where + N'AND TransactionType = @_TransactionType ';
+IF @ReadingTimeMax IS NOT NULL
+    SET @Where = @Where + N'AND ReadingTimeEstimate <= @_ReadingTimeMax ';
+
 IF LEN(@Where) > 0
-SET @sSQL = @sSQL + 'WHERE ' + RIGHT(@Where, LEN(@Where)-3)
- 
+    SET @sSQL = @sSQL + N'WHERE ' + RIGHT(@Where, LEN(@Where) - 3);
+
 EXEC sp_executesql @sSQL,
-N'@_Product int, @_OrderID int, @_TransactionType char(1), @_Qty int',
-@_Author = @AuthorName, @_OrderID = @OrderID, @_TransactionType = @TransactionType, @_Qty = @Qty
- 
+                   N'@_Product int, @_OrderID int, @_TransactionType char(1), @_Qty int',
+                   @_Author = @AuthorName,
+                   @_ArticleTitle = @ArticleTitle,
+                   @_ArticleDesc = @ArticleDesc,
+                   @_ReadingTimeMax = @ReadingTimeMax;
+
 GO
