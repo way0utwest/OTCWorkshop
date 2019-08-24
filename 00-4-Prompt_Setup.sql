f37d3ba4d554de57c40170adbbf1c29dcd6bac55
Steve Jones <sjones@sqlservercentral.com>
2019-08-23 18:12:45 -0600

    added search procedure (partial)

--- 00-4-Prompt_Setup.sql
+++ 00-4-Prompt_Setup.sql
@@ -53,3 +53,26 @@ WHERE PublishDate >= @startdate
 GROUP BY ContactID;
 
 GO
+CREATE PROCEDURE SearchArticles (@AuthorName VARCHAR(200) = NULL, @ArticleTitle VARCHAR(142) = NULL, @ArticleDesc varchar(1000) = NULL, @ReadingTimeMax int = NULL)
+AS
+DECLARE @sSQL NVARCHAR(2000), @Where NVARCHAR(1000) = ''
+SET @sSQL = 'SELECT ProductID, ReferenceOrderID, TransactionType, Quantity, TransactionDate, ActualCost
+from dbo.Articles as a inner join dbo.Contacts as c on a.authorid = c.contactsid '
+ 
+IF @AuthorName is not null
+SET @Where = @Where + 'AND c.ContactFullName like @_Author '
+IF @OrderID is not null
+SET @Where = @Where + 'AND ReferenceOrderID = @_OrderID '
+IF @TransactionType IS NOT NULL
+SET @Where = @Where + 'AND TransactionType = @_TransactionType '
+IF @Qty IS NOT NULL
+SET @Where = @Where + 'AND Quantity = @_Qty '
+ 
+IF LEN(@Where) > 0
+SET @sSQL = @sSQL + 'WHERE ' + RIGHT(@Where, LEN(@Where)-3)
+ 
+EXEC sp_executesql @sSQL,
+N'@_Product int, @_OrderID int, @_TransactionType char(1), @_Qty int',
+@_Author = @AuthorName, @_OrderID = @OrderID, @_TransactionType = @TransactionType, @_Qty = @Qty
+ 
+GO
